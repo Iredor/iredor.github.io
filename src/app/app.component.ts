8c04bd6825ddc603fb3a810d93d57d61af39a3af
Iredor <mascolo.pietro93@gmail.com>
2023-05-27 10:41:36 +0200

    Second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,43 +10,32 @@ export class AppComponent {
   shouldShowThreeButtons: boolean = false;
   chose: string = 'home';
   drink: string[] = ['Drink a shot','Drink two shots','Drink in one gulp','Blowjob shot','Drink from the bottle','Drink from his body'];
-  soft: string[] = ['Kiss on the lips', 'Touch the ass', 'Show ass','Show them','Show boobs','Sexy striptease','Find honey on her body while blindfolded'];
-  hard: string[] = ['Oral sex', 'Masturbate', 'Masturbate him', '69', 'Doggy style',
-  'Seated scissors', 'Spork sex', 'Standing oral sex', 'See saw sex', 'Ass fingering', 'Crazy starfish',
-  'Chair blowjob','Ass licking','Lap dance sex','Have fun with a sex toy','Nipples licking','Oral sex with ass fingering','Sex on a bar stool',
-  'Oral sex while masturbating','Masturbate each other','Anal sex - If you agree','Double penetration with sex toy','Guess 3 ingredients from her private parts','Bondage'];
+  soft: string[] = ['Kiss on the lips', 'Touch the ass',  'Show ass','Show them','Show boobs','Sexy striptease',
+                    'Find honey on her body while blindfolded'];
+  hard: string[] = ['Doggy style', 'Seated scissors', 'Spork sex','See saw sex', 'Crazy starfish', 'Lap dance sex', 
+                    'Sex on a bar stool', 'Anal sex - If you agree','Double penetration with sex toy'];
+  medium: string[] = ['Oral sex', 'Standing oral sex', 'Ass fingering', 'Chair blowjob' ,'Masturbate','Have fun with a sex toy',
+                      'Nipples licking', 'Masturbate him', '69', 'Ass licking', 'Oral sex with ass fingering',
+                      'Oral sex while masturbating','Masturbate each other','Bondage', 'Guess 3 ingredients from her private parts'];
   randomElement: string = 'home';
-  test:boolean = false;
   tot:number =0;
+  totHard:number =0;
 
   showThreeButtons(choosing: string) {
     this.chose = choosing;
   }
 
-  count(){
+  countMedium(){
     this.tot = this.tot + 1;
   }
 
-  getImgUrl(list: string[]) {
+  countHard(){
+    this.totHard = this.totHard + 1;
+  }
+
+  getImgUrl(list: string[]) {    
     this.randomElement = list[Math.floor(Math.random() * list.length)];
     return '../assets/img/' + this.randomElement + '.png';
-  }
-
-  timeLeft: number = 60;
-  interval: any;
-
-startTimer() {
-    this.interval = setInterval(() => {
-      if(this.timeLeft > 0) {
-        this.timeLeft--;
-      } else {
-        this.timeLeft = 60;
-      }
-    },1000)
-  }
-
-  pauseTimer() {
-    clearInterval(this.interval);
   }
     
 }
